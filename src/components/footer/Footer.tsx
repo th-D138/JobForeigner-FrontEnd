b53130731af69ffda5f4ce0100c5f52a4d5cbcf3
oko_jin <dswvgw6412@naver.com>
2025-01-14 19:36:13 +0900

    feat: 푸터 UI 구현

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,26 @@
+import { Link } from 'react-router-dom';
+import styles from './footer.module.scss';
+
+export default function Footer() {
+  return (
+    <footer className={styles.footer}>
+      <div className={styles.links}>
+        <Link to='/'>광고문의</Link>
+        <Link to='/'>제휴문의</Link>
+      </div>
+      <div className={styles.info}>
+        <div>
+          고객센터 : 054-578-0000 (평일 09:00 ~ 18:00) | Fax : 054-578-0000 |
+          Email : anten@kumoh.ac.kr
+        </div>
+        <div>
+          대표 : 안형태 | 사업자등록번호 : 01-13-12923-401 | 주소 : 경상북도
+          구미시 대학로 61 D139
+        </div>
+      </div>
+      <div className={styles.info}>
+        © JobForeigner LLC. All rights reserved.
+      </div>
+    </footer>
+  );
+}
