4ec447b08b6a0edc5479ccd5775504ed32eb7211
oko_jin <dswvgw6412@naver.com>
2025-01-14 15:03:32 +0900

    feat: 페이지 라우터 적용

--- src/router.tsx
+++ src/router.tsx
@@ -3,8 +3,18 @@ import {
   createRoutesFromElements,
   Route,
 } from 'react-router-dom';
-import MainPage from './pages/MainPage';
+import MainPage from './pages/main/Page';
+import CommunityPage from './pages/community/Page';
+import CorporateInfoPage from './pages/corporateInfo/Page';
+import NotFoundPage from './pages/notFound/Page';
 
 export const router = createBrowserRouter(
-  createRoutesFromElements(<Route path='/' element={<MainPage />} />)
+  createRoutesFromElements(
+    <Route path='/'>
+      <Route path='' element={<MainPage />} />
+      <Route path='community' element={<CommunityPage />} />
+      <Route path='corporateInfo' element={<CorporateInfoPage />} />
+      <Route path='*' element={<NotFoundPage />} />
+    </Route>
+  )
 );
