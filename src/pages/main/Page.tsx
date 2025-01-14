6fd67ff6fa46de443e475eb9e9035d4320a676ca
oko_jin <dswvgw6412@naver.com>
2025-01-14 19:50:03 +0900

    feat: 헤더 푸터 적용

--- src/pages/main/Page.tsx
+++ src/pages/main/Page.tsx
@@ -1,10 +1,13 @@
+import Footer from '@/components/footer/Footer';
 import Header from '../../components/header/Header';
+import styles from './page.module.scss';
 
 export default function MainPage() {
   return (
-    <>
+    <div>
       <Header />
-      <div>메인 페이지</div>
-    </>
+      <div className={styles.page}>메인 페이지</div>
+      <Footer />
+    </div>
   );
 }
