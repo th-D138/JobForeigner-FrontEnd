0977150144c1fa3bd9d5935b8d35feb3f913fde8
oko_jin <dswvgw6412@naver.com>
2025-03-03 17:19:47 +0900

    feat: 메인 페이지 배너 UI 퍼블리싱
    #3

--- src/pages/main/Page.tsx
+++ src/pages/main/Page.tsx
@@ -1,12 +1,15 @@
 import Footer from '@/components/footer/Footer';
 import Header from '../../components/header/Header';
 import styles from './page.module.scss';
+import Banner from '@/components/main/Banner';
 
 export default function MainPage() {
   return (
     <div>
       <Header />
-      <div className={styles.page}>메인 페이지</div>
+      <main className={styles.page}>
+        <Banner />
+      </main>
       <Footer />
     </div>
   );
