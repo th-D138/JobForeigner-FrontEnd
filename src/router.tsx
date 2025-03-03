439e236f0156b9c02ec697cda1846cad3389b60b
oko_jin <dswvgw6412@naver.com>
2025-03-03 16:03:55 +0900

    feat: 페이지 동적 로딩 구현

--- src/router.tsx
+++ src/router.tsx
@@ -3,18 +3,29 @@ import {
   createRoutesFromElements,
   Route,
 } from 'react-router-dom';
-import MainPage from './pages/main/Page';
-import CommunityPage from './pages/community/Page';
-import CompaniesPage from './pages/companies/Page';
-import NotFoundPage from './pages/notFound/Page';
+import { lazy } from 'react';
+
+// 만든 HOC 임포트
+import withSuspense from '@/pages/withSuspense';
+
+const MainPage = lazy(() => import('./pages/main/Page'));
+const CommunityPage = lazy(() => import('./pages/community/Page'));
+const CompaniesPage = lazy(() => import('./pages/companies/Page'));
+const NotFoundPage = lazy(() => import('./pages/notFound/Page'));
+
+// 각 페이지를 HOC로 감싸기
+const SuspensedMainPage = withSuspense(MainPage);
+const SuspensedCommunityPage = withSuspense(CommunityPage);
+const SuspensedCompaniesPage = withSuspense(CompaniesPage);
+const SuspensedNotFoundPage = withSuspense(NotFoundPage);
 
 export const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path='/'>
-      <Route path='' element={<MainPage />} />
-      <Route path='community' element={<CommunityPage />} />
-      <Route path='companies' element={<CompaniesPage />} />
-      <Route path='*' element={<NotFoundPage />} />
+    <Route path="/">
+      <Route path="" element={<SuspensedMainPage />} />
+      <Route path="community" element={<SuspensedCommunityPage />} />
+      <Route path="companies" element={<SuspensedCompaniesPage />} />
+      <Route path="*" element={<SuspensedNotFoundPage />} />
     </Route>
   )
 );
