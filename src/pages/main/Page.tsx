10073eadd2749ddb070acc8f13d20b03d1d498c7
oko_jin <dswvgw6412@naver.com>
2025-03-03 23:34:58 +0900

    fix: 헤더 푸터 제거

--- src/pages/main/Page.tsx
+++ src/pages/main/Page.tsx
@@ -1,18 +1,12 @@
-import Footer from '@/components/common/footer/Footer';
-import Header from '../../components/common/header/Header';
 import styles from './page.module.scss';
 import Banner from '@/components/main/Banner';
 import SearchSection from '@/components/main/SearchSection';
 
 export default function MainPage() {
   return (
-    <div>
-      <Header />
-      <main className={styles.page}>
-        <Banner />
-        <SearchSection />
-      </main>
-      <Footer />
-    </div>
+    <main className={styles.page}>
+      <Banner />
+      <SearchSection />
+    </main>
   );
 }
