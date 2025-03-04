e47bf2f58e10ec25fb5998facf1e2dbed0304ffd
okojin <dswvgw6412@naver.com>
2025-03-04 10:39:52 +0900

    feat: 채용 공고 섹션 UI 퍼블리싱
    #3

---
+++ src/components/main/JobCard.tsx
@@ -0,0 +1,50 @@
+import { Link } from 'react-router-dom';
+import styles from './jobCard.module.scss';
+import { MapPin } from 'lucide-react';
+
+export interface Props {
+  job: {
+    id: number;
+    title: string;
+    company: string;
+    companyLogo: string;
+    location: string;
+    isNew: boolean;
+    timeAgo: string;
+    tags: string[];
+  };
+}
+
+export default function JobCard({ job }: Props) {
+  return (
+    <Link to={`/companies`} className={styles.jobCard}>
+      <div className={styles.jobCardBody}>
+        <div className={styles.jobCardTop}>
+          <div className={styles.jobLogoWrapper}>
+            <img
+              src={job.companyLogo}
+              alt={job.company}
+              className={styles.jobLogo}
+            />
+          </div>
+          <span className={job.isNew ? styles.badgeNew : styles.badgeDefault}>
+            {job.isNew ? '신규' : job.timeAgo}
+          </span>
+        </div>
+        <h3 className={styles.jobTitle}>{job.title}</h3>
+        <p className={styles.companyName}>{job.company}</p>
+        <div className={styles.locationRow}>
+          <MapPin className={styles.locationIcon} />
+          <span>{job.location}</span>
+        </div>
+        <div className={styles.tagsWrapper}>
+          {job.tags.map((tag) => (
+            <span key={`id-${tag}`} className={styles.jobTag}>
+              {tag}
+            </span>
+          ))}
+        </div>
+      </div>
+    </Link>
+  );
+}
