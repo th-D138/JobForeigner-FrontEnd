296f471d325ab1f84baa75873605c1d94dbaa13f
okojin <dswvgw6412@naver.com>
2025-03-04 11:51:10 +0900

    feat: 검색 버튼 submit 타입 적용
    #3

--- src/components/common/header/SearchForm.tsx
+++ src/components/common/header/SearchForm.tsx
@@ -4,10 +4,10 @@ import styles from './searchForm.module.scss';
 export default function SearchForm() {
   return (
     <form className={styles.searchForm}>
-      <label className={styles.searchButton}>
-        <Search size={16} color='#b3b3b3' />
-      </label>
-      <input placeholder='채용 공고 검색하기' />
+      <button className={styles.searchButton} type="submit">
+        <Search size={16} color="#b3b3b3" />
+      </button>
+      <input name="search" placeholder="채용 공고 검색하기" />
     </form>
   );
 }
