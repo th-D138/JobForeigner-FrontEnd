57163ce68454e22d7f18a59908ab166ac91afa0d
oko_jin <dswvgw6412@naver.com>
2025-03-05 14:59:11 +0900

    feat: 마이페이지 라우팅 적용
    #7

--- src/router.tsx
+++ src/router.tsx
@@ -10,12 +10,14 @@ import Layout from './components/layout/Layout';
 import withSuspense from '@/pages/withSuspense';
 
 const MainPage = lazy(() => import('./pages/main/Page'));
+const ProfilePage = lazy(() => import('./pages/profile/Page'));
 const CommunityPage = lazy(() => import('./pages/community/Page'));
 const CompaniesPage = lazy(() => import('./pages/companies/Page'));
 const NotFoundPage = lazy(() => import('./pages/notFound/Page'));
 
 // 각 페이지를 Suspense가 적용된 HOC로 감싸기
 const SuspensedMainPage = withSuspense(MainPage);
+const SuspensedProfilePage = withSuspense(ProfilePage);
 const SuspensedCommunityPage = withSuspense(CommunityPage);
 const SuspensedCompaniesPage = withSuspense(CompaniesPage);
 const SuspensedNotFoundPage = withSuspense(NotFoundPage);
@@ -24,6 +26,7 @@ export const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<Layout />}>
       <Route index element={<SuspensedMainPage />} />
+      <Route path="profile" element={<SuspensedProfilePage />} />
       <Route path="community" element={<SuspensedCommunityPage />} />
       <Route path="companies" element={<SuspensedCompaniesPage />} />
       <Route path="*" element={<SuspensedNotFoundPage />} />
