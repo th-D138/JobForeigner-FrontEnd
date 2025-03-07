7f0ce2fc9ca748299d74823b248aec7e909c981a
oko_jin <dswvgw6412@naver.com>
2025-03-07 17:06:06 +0900

    feat: 마이페이지 UI 적용
    #7

--- src/components/profile/UserProfile.tsx
+++ src/components/profile/UserProfile.tsx
@@ -1,55 +1,29 @@
 import { ProfileImage, ProfileInfo, ProfileBottom } from '../profile';
 import styles from './userProfile.module.scss';
 
-const userInfo = {
-  profileImageUrl: 'https://randomuser.me/api',
-  name: '홍길동',
-  email: 'dswvgw1234@gmail.com',
-  phoneNumber: '010-1234-5678',
-  region: '서울특별시 강남구',
-  resumes: [
-    {
-      id: 1,
-      title: '이력서1',
-      createdAt: '2021-08-01',
-      updatedAt: '2021-08-01',
-    },
-    {
-      id: 2,
-      title: '이력서2',
-      createdAt: '2021-08-01',
-      updatedAt: '2021-08-01',
-    },
-    {
-      id: 3,
-      title: '이력서3',
-      createdAt: '2021-08-01',
-      updatedAt: '2021-08-01',
-    },
-  ],
-  applications: [
-    {
-      id: 1,
-      title: '회사1',
-      createdAt: '2021-08-01',
-      updatedAt: '2021-08-01',
-    },
-    {
-      id: 2,
-      title: '회사2',
-      createdAt: '2021-08-01',
-      updatedAt: '2021-08-01',
-    },
-    {
-      id: 3,
-      title: '회사3',
-      createdAt: '2021-08-01',
-      updatedAt: '2021-08-01',
-    },
-  ],
-};
+interface Props {
+  userInfo: {
+    name: string;
+    email: string;
+    phoneNumber: string;
+    region: string;
+    profileImageUrl?: string;
+    resumes: {
+      id: number;
+      title: string;
+      createdAt: string;
+      updatedAt: string;
+    }[];
+    applications: {
+      id: number;
+      title: string;
+      createdAt: string;
+      updatedAt: string;
+    }[];
+  };
+}
 
-export default function UserProfile() {
+export default function UserProfile({ userInfo }: Props) {
   return (
     <div className={styles.profileCard}>
       <div className={styles.profileCardBody}>
--- src/pages/profile/Page.tsx
+++ src/pages/profile/Page.tsx
@@ -1,23 +1,74 @@
-import UserProfile from '@/components/profile/UserProfile';
+import { UserProfile, ResumeList } from '@/components/profile';
 
 import styles from './page.module.scss';
 
+const userInfo = {
+  profileImageUrl: 'https://randomuser.me/api',
+  name: '홍길동',
+  email: 'dswvgw1234@gmail.com',
+  phoneNumber: '010-1234-5678',
+  region: '서울특별시 강남구',
+  resumes: [
+    {
+      id: 1,
+      title: '이력서1',
+      status: '작성완료',
+      createdAt: '2021-08-01',
+      updatedAt: '2021-08-01',
+    },
+    {
+      id: 2,
+      title: '이력서2',
+      status: '작성중',
+      createdAt: '2021-08-01',
+      updatedAt: '2021-08-01',
+    },
+    {
+      id: 3,
+      title: '이력서3',
+      status: '작성완료',
+      createdAt: '2021-08-01',
+      updatedAt: '2021-08-01',
+    },
+  ],
+  applications: [
+    {
+      id: 1,
+      title: '회사1',
+      createdAt: '2021-08-01',
+      updatedAt: '2021-08-01',
+    },
+    {
+      id: 2,
+      title: '회사2',
+      createdAt: '2021-08-01',
+      updatedAt: '2021-08-01',
+    },
+    {
+      id: 3,
+      title: '회사3',
+      createdAt: '2021-08-01',
+      updatedAt: '2021-08-01',
+    },
+  ],
+};
+
 export default function ProfilePage() {
   return (
-    <div className={styles.layout}>
-      <div>sidebar</div>
-      <div className={styles.container}>
-        <main className={styles.page}>
-          <h1>프로필</h1>
-          <UserProfile />
-          <section>
-            <h2>내 이력서</h2>
-          </section>
-          <section>
-            <h2>지원 내역</h2>
-          </section>
-        </main>
-      </div>
+    <div className={styles.container}>
+      <main className={styles.page}>
+        <h1>프로필</h1>
+        <UserProfile userInfo={userInfo} />
+        <section>
+          <h2>내 이력서</h2>
+          <ResumeList resumes={userInfo.resumes}>
+            <ResumeList.items resumes={userInfo.resumes} />
+          </ResumeList>
+        </section>
+        <section>
+          <h2>지원 내역</h2>
+        </section>
+      </main>
     </div>
   );
 }
