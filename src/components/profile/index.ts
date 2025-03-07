c86cd33f0898840d47923c2ef483f0a569032613
oko_jin <dswvgw6412@naver.com>
2025-03-07 17:05:49 +0900

    feat: barrel export 패턴 적용
    #7

--- src/components/profile/index.ts
+++ src/components/profile/index.ts
@@ -2,3 +2,4 @@ export { default as ProfileBottom } from './ProfileBottom';
 export { default as ProfileImage } from './ProfileImage';
 export { default as ProfileInfo } from './ProfileInfo';
 export { default as UserProfile } from './UserProfile';
+export { default as ResumeList } from './ResumeList';
