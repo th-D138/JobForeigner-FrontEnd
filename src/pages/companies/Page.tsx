e600961b742a8b26826f129d9fb51021e4308ada
minwoo1119 <84209687+minwoo1119@users.noreply.github.com>
2025-03-09 17:35:41 +0900

    feat : DropDown 컴포넌트 작성 및 기업정보 페이지 DropDown 적용
    #5

--- src/pages/companies/Page.tsx
+++ src/pages/companies/Page.tsx
@@ -1,9 +1,12 @@
+import styles from "./page.module.scss";
 import SearchBar from "@/components/companies/SearchBar";
+import SearchFilterBar from "@/components/companies/SearchFilterBar";
 
 export default function CompaniesPage() {
 	return (
-		<>
+		<div className={styles.container}>
 			<SearchBar />
-		</>
+			<SearchFilterBar />
+		</div>
 	);
 }
