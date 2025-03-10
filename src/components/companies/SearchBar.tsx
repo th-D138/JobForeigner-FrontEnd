8fe6b02c8676d46afc6ad85fcd75f2260971c866
minwoo1119 <84209687+minwoo1119@users.noreply.github.com>
2025-03-10 22:39:15 +0900

    style: 검색 창 스타일 수정

--- src/components/companies/SearchBar.tsx
+++ src/components/companies/SearchBar.tsx
@@ -3,16 +3,16 @@ import styles from "./searchBar.module.scss";
 
 const SearchBar = () => {
 	return (
-		<section className={styles.searchBarSection}>
-			<div className={styles.container}>
-				<input
-					type="text"
-					placeholder="기업명으로 검색하세요"
-					className={styles.searchInput}
-				/>
+		<div className={styles.container}>
+			<input
+				type="text"
+				placeholder="기업명으로 검색하세요"
+				className={styles.searchInput}
+			/>
+			<div className={styles.searchIcon}>
 				<Search />
 			</div>
-		</section>
+		</div>
 	);
 };
 
