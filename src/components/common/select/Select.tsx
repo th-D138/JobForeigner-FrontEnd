53f82b347ead4da98891f31c87e28b37ac5c4160
oko_jin <dswvgw6412@naver.com>
2025-03-10 14:39:48 +0900

    refactor: Select 컴포넌트 리팩토링
    #9

---
+++ src/components/common/select/Select.tsx
@@ -0,0 +1,92 @@
+import { useState, useRef, useEffect } from 'react';
+import styles from './select.module.scss';
+import { MapPin, Search } from 'lucide-react';
+
+const getIcon = (icon?: string) => {
+  switch (icon) {
+    case 'search':
+      return <Search className={styles.icon} />;
+    case 'map-pin':
+      return <MapPin className={styles.icon} />;
+    default:
+      return null;
+  }
+};
+
+interface Option {
+  value: string;
+  label: string;
+}
+
+interface SelectProps {
+  icon?: string;
+  options: Option[];
+  defaultValue?: string;
+  onChange?: (value: string) => void;
+  name?: string;
+}
+
+export default function Select({
+  icon,
+  options,
+  defaultValue,
+  onChange,
+  name,
+  ...props
+}: SelectProps) {
+  const [selectedValue, setSelectedValue] = useState(
+    defaultValue || options[0]?.value,
+  );
+  const [isOpen, setIsOpen] = useState(false);
+  const wrapperRef = useRef<HTMLDivElement>(null);
+
+  const selectedOption = options.find(opt => opt.value === selectedValue);
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (
+        wrapperRef.current &&
+        !wrapperRef.current.contains(event.target as Node)
+      ) {
+        setIsOpen(false);
+      }
+    };
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => document.removeEventListener('mousedown', handleClickOutside);
+  }, []);
+
+  const handleOptionClick = (optionValue: string) => {
+    setSelectedValue(optionValue);
+    if (onChange) {
+      onChange(optionValue);
+    }
+    setIsOpen(false);
+  };
+
+  return (
+    <div className={styles.selectWrapper} ref={wrapperRef} {...props}>
+      {getIcon(icon)}
+      <div
+        className={styles.customSelect}
+        onClick={() => setIsOpen(prev => !prev)}
+        tabIndex={0}
+      >
+        {selectedOption ? selectedOption.label : '옵션 선택'}
+      </div>
+      {isOpen && (
+        <ul className={styles.optionsList}>
+          {options.map(opt => (
+            <li
+              key={opt.value}
+              className={styles.option}
+              onClick={() => handleOptionClick(opt.value)}
+            >
+              {opt.label}
+            </li>
+          ))}
+        </ul>
+      )}
+      {name && <input type='hidden' name={name} value={selectedValue} />}
+    </div>
+  );
+}
