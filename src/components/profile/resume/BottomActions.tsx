e2b983034c90fb3b40976af27dfb481b014c4d12
oko_jin <dswvgw6412@naver.com>
2025-03-14 17:42:20 +0900

    feat: progress 바 기능 구현
    #11

--- src/components/profile/resume/BottomActions.tsx
+++ src/components/profile/resume/BottomActions.tsx
@@ -2,10 +2,31 @@ import Progress from '@/components/common/progress/Progress';
 import styles from './bottomActions.module.scss';
 import Button from '@/components/common/button/Button';
 import { FileDown, Save } from 'lucide-react';
-
-const progress = 80;
+import { useFormContext } from 'react-hook-form';
+import { useMemo } from 'react';
 
 export default function BottomActions() {
+  const { watch } = useFormContext();
+  const formValues = watch();
+
+  const progress = useMemo(() => {
+    const requiredFields = [
+      'title',
+      'name',
+      'email',
+      'phoneNumber',
+      'sido',
+      'sigungu',
+      'job',
+    ];
+
+    const totalFields = requiredFields.length;
+    const filledFields = requiredFields.filter(
+      field => formValues[field] && formValues[field].toString().trim() !== '',
+    ).length;
+
+    return Math.round((filledFields / totalFields) * 100);
+  }, [formValues]);
   return (
     <div className={styles.bottomBar}>
       <div className={styles.bottomBarContainer}>
