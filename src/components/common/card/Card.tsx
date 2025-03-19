4d8d9fa6ab8bb06fe457e99c8a5ed16c2b7cd25e
oko_jin <dswvgw6412@naver.com>
2025-03-19 14:41:49 +0900

    docs: 카드 컴포넌트 사용법 JSDoc으로 정리
    #20

--- src/components/common/card/Card.tsx
+++ src/components/common/card/Card.tsx
@@ -4,6 +4,12 @@ interface CardProps extends React.HTMLAttributes<HTMLDivElement> {
   children: React.ReactNode;
 }
 
+/**
+ * - 카드 컴포넌트
+ *   - 카드 모양으로 특정 컴포넌트를 감쌀 때 사용
+ * - children: 카드 내용
+ * - 나머지 props: (onClick, children 등) HTMLAttributes(div 태그의 속성)
+ */
 export default function Card({ children, ...props }: CardProps) {
   return (
     <div className={styles.card} {...props}>
