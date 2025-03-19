a775994b3a9f1bb83801f09ab94c252d235f36d1
oko_jin <dswvgw6412@naver.com>
2025-03-19 14:51:15 +0900

    refactor: 에러 메시지, 정규식 리팩토링
    #20

---
+++ src/lib/schemas/error.ts
@@ -0,0 +1,18 @@
+export const ERROR_MSG = {
+  required: '필수 입력 항목입니다.',
+  numberRequired: '1 이상의 숫자를 입력해주세요.',
+  password: {
+    min: '8자 이상으로 입력해주세요.',
+    confirm: '비밀번호가 일치하지 않습니다.',
+  },
+  exceed: {
+    ten: '10자 이내로 입력해주세요.',
+    thirty: '30자 이내로 입력해주세요.',
+    fifty: '50자 이내로 입력해주세요.',
+    hundred: '100자 이내로 입력해주세요.',
+    thousand: '1000자 이내로 입력해주세요.',
+    twoThousand: '2000자 이내로 입력해주세요.',
+    fiveThousand: '5000자 이내로 입력해주세요.',
+  },
+  phoneNumber: '올바른 전화번호를 입력해주세요.',
+};
--- src/lib/schemas/resumeSchema.ts
+++ src/lib/schemas/resumeSchema.ts
@@ -1,23 +1,6 @@
 import { z } from 'zod';
-
-const REGEX = {
-  phoneNumber: /^\d{2,3}-\d{3,4}-\d{4}$/,
-};
-
-const ERROR_MSG = {
-  required: '필수 입력 항목입니다.',
-  numberRequired: '1 이상의 숫자를 입력해주세요.',
-  exceed: {
-    ten: '10자 이내로 입력해주세요.',
-    thirty: '30자 이내로 입력해주세요.',
-    fifty: '50자 이내로 입력해주세요.',
-    hundred: '100자 이내로 입력해주세요.',
-    thousand: '1000자 이내로 입력해주세요.',
-    twoThousand: '2000자 이내로 입력해주세요.',
-    fiveThousand: '5000자 이내로 입력해주세요.',
-  },
-  phoneNumber: '올바른 전화번호를 입력해주세요.',
-};
+import { ERROR_MSG } from './error';
+import { REGEX } from './regex';
 
 export const resumeSchema = z.object({
   title: z.string().min(1, ERROR_MSG.required).max(50, ERROR_MSG.exceed.fifty),
