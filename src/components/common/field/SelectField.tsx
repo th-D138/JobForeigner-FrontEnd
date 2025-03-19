7f2ec10ed34ced67064cdbe7c56f84a9bbf9726d
oko_jin <dswvgw6412@naver.com>
2025-03-19 14:46:05 +0900

    docs: SelectField 컴포넌트 사용법 JSDoc 정리
    #20

--- src/components/common/field/SelectField.tsx
+++ src/components/common/field/SelectField.tsx
@@ -15,6 +15,18 @@ type Props = {
   }[];
 };
 
+/**
+ * - React Hook Form 선택 필드 컴포넌트
+ *   - 선택 필드 타입 지원
+ *   - 필수 입력 칸 입니다. 메시지 표시 지원
+ * - control: 폼 제어 객체
+ * - name: 필드 이름
+ * - label: 필드 레이블
+ * - required: 필수 입력 여부
+ * - options: 선택 필드 옵션 배열({value: string, label: string}[])
+ *   - value: 옵션 값
+ *   - label: 옵션 레이블
+ */
 export default function SelectField({
   control,
   name,
