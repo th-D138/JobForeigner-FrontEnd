496e84a76667c9d21e77e62f08149973f2c77a73
oko_jin <dswvgw6412@naver.com>
2025-03-27 15:25:14 +0900

    feat: i18n 작성

---
+++ i18n/i18n.ts
@@ -0,0 +1,21 @@
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+
+import enCommon from '../public/locales/en/common.json';
+import koCommon from '../public/locales/ko/common.json';
+
+const resources = {
+  en: { common: enCommon },
+  ko: { common: koCommon },
+};
+
+i18n.use(initReactI18next).init({
+  resources,
+  lng: 'ko',
+  fallbackLng: 'en',
+  interpolation: {
+    escapeValue: false,
+  },
+});
+
+export default i18n;
