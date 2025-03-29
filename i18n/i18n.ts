8719a4607fbe2d764f4985ce6be25e8d3f83cd00
oko_jin <dswvgw6412@naver.com>
2025-03-29 15:25:20 +0900

    feat: mainPage.json을 i18n에 적용
    #25

--- i18n/i18n.ts
+++ i18n/i18n.ts
@@ -2,17 +2,27 @@ import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 
 import enCommon from '../locales/en/common.json';
+import enMainPage from '../locales/en/mainPage.json';
 import koCommon from '../locales/ko/common.json';
+import koMainPage from '../locales/ko/mainPage.json';
 
 const resources = {
-  en: { common: enCommon },
-  ko: { common: koCommon },
+  en: {
+    common: enCommon,
+    mainPage: enMainPage,
+  },
+  ko: {
+    common: koCommon,
+    mainPage: koMainPage,
+  },
 };
 
 i18n.use(initReactI18next).init({
   resources,
   lng: 'ko',
   fallbackLng: 'en',
+  ns: ['common', 'mainPage'],
+  defaultNS: 'common',
   interpolation: {
     escapeValue: false,
   },
