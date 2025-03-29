1e9aa36bfecc73a4909373df0289bb75f96aa4ea
minwoo1119 <84209687+minwoo1119@users.noreply.github.com>
2025-03-29 17:17:16 +0900

    feat: 채용 공고 상세 페이지 지원 Bar 구현

--- src/pages/jobs/DetailPage.tsx
+++ src/pages/jobs/DetailPage.tsx
@@ -5,6 +5,7 @@ import { useLocation } from 'react-router-dom';
 import { RecruitInfoType } from '@/components/jobs/RecruitBar';
 import AdvertiseRecruitBox from '@/components/jobs/AdvertiseRecruitBox';
 import RecruitContent from '@/components/jobs/RecruitContent';
+import ApplyTab from '@/components/jobs/ApplyTab';
 
 const DetailPage = () => {
   const location = useLocation();
@@ -14,6 +15,7 @@ const DetailPage = () => {
       <DetailInfoBox {...data} />
       <AdvertiseRecruitBox />
       <RecruitContent />
+      <ApplyTab key={data.id} recruitId={data.id} />
     </div>
   );
 };
