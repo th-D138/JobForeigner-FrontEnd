b856a1339596bcdd28a02f3a07242e57d4bca67b
dswvgw6412 <dswvgw6412@naver.com>
2025-05-08 23:33:54 +0900

    feat: 유저 프로필 수정 페이지 UI 구현
    #51

---
+++ src/components/profile/edit/UserProfileEditForm.tsx
@@ -0,0 +1,86 @@
+import useImageUpload from '@/lib/hooks/useImageUpload';
+import styles from './userProfileEditForm.module.scss';
+import { useFormContext } from 'react-hook-form';
+import Button from '@/components/common/button/Button';
+import { Camera, Save } from 'lucide-react';
+import InputField from '@/components/common/field/InputField';
+
+export default function UserProfileEditForm() {
+  const { control, setValue } = useFormContext();
+  const { image, fileInputRef, handleImageUpload, handleUploadClick } =
+    useImageUpload(setValue);
+
+  return (
+    <div className={styles.formWrapper}>
+      <div className={styles.container}>
+        <div className={styles.imageWrapper}>
+          <div className={styles.imageContainer}>
+            {image ? (
+              <img src={image} alt='profileImage' className={styles.image} />
+            ) : (
+              <div className={styles.placeholder}></div>
+            )}
+            <input
+              type='file'
+              accept='image/*'
+              ref={fileInputRef}
+              onChange={handleImageUpload}
+              hidden
+            />
+          </div>
+          <button
+            type='button'
+            className={styles.button}
+            onClick={handleUploadClick}
+          >
+            <Camera className={styles.icon} />
+          </button>
+        </div>
+        <p className={styles.caption}>권장 크기: 400x400 픽셀, 최대 2MB</p>
+      </div>
+      <div className={styles.twoRow}>
+        <InputField
+          control={control}
+          name='email'
+          label='이메일'
+          required={true}
+        />
+        <InputField
+          control={control}
+          name='phoneNumber'
+          label='전화번호'
+          required={true}
+        />
+      </div>
+      <InputField
+        control={control}
+        name='address'
+        label='주소'
+        required={true}
+      />
+      <div className={styles.twoRow}>
+        <InputField
+          control={control}
+          name='detailAddress'
+          label='상세주소'
+          required={true}
+        />
+        <InputField
+          control={control}
+          name='zipcode'
+          label='우편번호'
+          required={true}
+        />
+      </div>
+
+      <div className={styles.actions}>
+        <Button size='medium'>
+          <span className={styles.buttonText}>
+            <Save />
+            저장하기
+          </span>
+        </Button>
+      </div>
+    </div>
+  );
+}
