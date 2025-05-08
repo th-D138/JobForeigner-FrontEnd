7e071905447a2333b07367f741d7dec3630c9c6b
dswvgw6412 <dswvgw6412@naver.com>
2025-05-08 22:47:06 +0900

    feat: 페이지 초기 커밋

---
+++ src/pages/profile/edit/Page.tsx
@@ -0,0 +1,9 @@
+import styles from './page.module.scss';
+
+export default function ProfileEditPage() {
+  return (
+    <div className={styles.container}>
+      <div>유저 프로필 수정 페이지</div>
+    </div>
+  );
+}
