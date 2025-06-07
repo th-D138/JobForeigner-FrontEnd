fcb71c9330ccfdd5974dd9366465d43a6558c0fc
dswvgw6412 <dswvgw6412@naver.com>
2025-06-07 18:52:14 +0900

    chore: 안쓰는 파일 제거

--- src/lib/constants/index.ts
+++ src/lib/constants/index.ts
@@ -3,7 +3,6 @@ import { PATH } from './routes';
 import { LOCAL_STORAGE } from './storage';
 
 export * from './navItems';
-export * from './registerSelectForm';
 export { PATH, title, LOCAL_STORAGE };
 
 export const API_URL = import.meta.env.VITE_BASE_API_URL as string;
